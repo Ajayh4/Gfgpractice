968d41faccfcb57fe8b88d91d64f8bddd512d37b
Ajayh4 <84365759+Ajayh4@users.noreply.github.com>
2023-07-13 06:52:33 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Unique Number of Occurrences/unique-number-of-occurrences.java
@@ -0,0 +1,81 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+
+class IntArray
+{
+    public static int[] input(BufferedReader br, int n) throws IOException
+    {
+        String[] s = br.readLine().trim().split(" ");
+        int[] a = new int[n];
+        for(int i = 0; i < n; i++)
+            a[i] = Integer.parseInt(s[i]);
+
+        return a;
+    }
+
+    public static void print(int[] a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+
+    public static void print(ArrayList<Integer> a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while(t-- > 0){
+            
+            int n;
+            n = Integer.parseInt(br.readLine());
+            
+            
+            int[] arr = IntArray.input(br, n);
+            
+            Solution obj = new Solution();
+            boolean res = obj.isFrequencyUnique(n, arr);
+            
+            int _result_val = (res) ? 1 : 0;
+            System.out.println(_result_val);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+
+class Solution {
+    public static boolean isFrequencyUnique(int n, int[] arr) {
+        // code here
+        HashMap<Integer,Integer> hm=new HashMap<>();
+        for(int i: arr)
+        {
+            if(hm.containsKey(i))
+            hm.put(i,hm.get(i)+1);
+            else
+            hm.put(i,1);
+        }
+        HashMap<Integer,Integer> hm1=new HashMap<>();
+        for(int i: hm.keySet())
+        {
+            if(hm1.containsKey(hm.get(i)))
+            return false;
+            else
+            hm1.put(hm.get(i),1);
+        }
+        return true;
+    }
+}
+        
