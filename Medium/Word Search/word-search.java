5f47d6d331a4bd0326cd12f76e3ac1d11dc99ffa
Ajayh4 <84365759+Ajayh4@users.noreply.github.com>
2023-08-29 15:05:39 +0530

    Updated Solution - GfG to GitHub

--- Medium/Word Search/word-search.java
+++ Medium/Word Search/word-search.java
@@ -1,40 +1,38 @@
 //{ Driver Code Starts
+//Initial Template for Java
+
 import java.util.*;
 import java.lang.*;
+import java.math.*;
 import java.io.*;
-class GFG
-{
-    public static void main(String[] args) throws IOException
-    {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine().trim());
-        while(T-->0)
-        {
-            String[] S = br.readLine().trim().split(" ");
-            int n = Integer.parseInt(S[0]);
-            int m = Integer.parseInt(S[1]);
-            char [][] board = new char[n][m];
-            for(int i = 0; i < n; i++){
-                String[] s = br.readLine().trim().split(" ");
-                for(int j = 0; j < m; j++){
-                    board[i][j] = s[j].charAt(0);
-                }
-            }
-            String word = br.readLine().trim();
-            Solution obj = new Solution();
-            boolean ans = obj.isWordExist(board, word);
-            if(ans)
-                System.out.println("1");
-            else
-                System.out.println("0");
-       }
+
+class GFG {
+  public static void main(String[] args) throws IOException {
+    Scanner sc = new Scanner(System.in);
+    int T = sc.nextInt();
+    while (T-- > 0) {
+      int n = sc.nextInt();
+      int m = sc.nextInt();
+      char a[][] = new char[n][m];
+      sc.nextLine();
+      for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++)
+         a[i][j]=sc.next().charAt(0);
+      }
+      String word=sc.next();
+      Solution obj = new Solution();
+      int ans = obj.wordSearch(a,word) ? 1 : 0;
+      System.out.println(ans);
     }
+  }
 }
+
 // } Driver Code Ends
 
 
-class Solution
-{
+//User function Template for Java
+
+class Solution {
     static boolean rec(int i,int j,int r,int c,char board[][],int v[][],int cnt,String word)
     {
         if(cnt==word.length())
@@ -60,10 +58,9 @@ class Solution
             return ans;
         }
     }
-    public boolean isWordExist(char[][] board, String word)
-    {
-        // Code here
-        int r=board.length,c=board[0].length;
+  public static boolean wordSearch(char board[][],String word) {
+    // code here
+    int r=board.length,c=board[0].length;
         for(int i=0;i<r;i++)
         {
             for(int j=0;j<c;j++)
@@ -78,5 +75,6 @@ class Solution
             }
         }
         return false;
-    }
+  }
 }
+     
