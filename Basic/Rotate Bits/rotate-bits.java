080b5e57ccb7b55bba546a93bca73c8944aba2a7
Ajayh4 <84365759+Ajayh4@users.noreply.github.com>
2023-09-20 23:33:06 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Rotate Bits/rotate-bits.java
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0)
+        {
+            int n = sc.nextInt();
+            int d = sc.nextInt();
+            
+            Solution ob = new Solution();
+            
+            ArrayList<Integer> res = ob.rotate (n, d);
+            System.out.println(res.get(0));
+            System.out.println(res.get(1));
+            
+           
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    
+    ArrayList<Integer> rotate(int N, int D)
+    {
+        ArrayList<Integer> a=new ArrayList<>();
+        int m = D%16;
+        int l = (N<<m|N>>(16-m))&65535;
+        int r = (N>>m|N<<(16-m))&65535;
+        a.add(l);
+        a.add(r);
+        return a;
+    }
+}
