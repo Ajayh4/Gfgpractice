b46e7f5f5bdd8dfc516a0c53d144f45893e211f5
Ajayh4 <84365759+Ajayh4@users.noreply.github.com>
2023-10-08 09:26:18 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Insert in a Sorted List/insert-in-a-sorted-list.java
@@ -0,0 +1,93 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+
+class Node{
+    int data;
+    Node next;
+    
+    Node(int x){
+        data = x;
+        next = null;
+    }
+    
+}
+class GFG{
+	static void printList(Node node) 
+	{ 
+		while (node != null) 
+		{ 
+			System.out.print(node.data + " "); 
+			node = node.next; 
+		} 
+		System.out.println(); 
+	}
+	
+    public static void main(String args[]) throws IOException { 
+        
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t > 0){
+            int n = sc.nextInt();
+            
+            Node head = new Node(sc.nextInt());
+            Node tail = head;
+            
+            for(int i=0; i<n-1; i++)
+            {
+                tail.next = new Node(sc.nextInt());
+                tail = tail.next;
+            }
+            
+            int k = sc.nextInt();
+            
+            Solution g = new Solution();
+            head = g.sortedInsert(head,k);
+            
+            printList(head); 
+            t--;
+        }
+    } 
+} 
+
+// } Driver Code Ends
+
+
+/*class Node
+    {
+        int data;
+        Node next;
+        Node(int d) {data = d; next = null; }
+    }*/
+
+// sortedInsert method should return the head of the modified linked list.
+class Solution {
+    Node sortedInsert(Node head1, int key) {
+        // Add your code here.
+        Node nn=new Node(key);
+        if(key<=head1.data)
+        {
+            nn.next=head1;
+            head1=nn;
+            return head1;
+        }
+        Node p=null,t=head1;
+        while(t.next!=null)
+        {
+            p=t;
+            t=t.next;
+            if(t.data>key)
+            {
+                p.next=nn;
+                nn.next=t;
+                return head1;
+            }
+        }
+        if(t.data<=key)
+        {
+            t.next=nn;
+        }
+        return head1;
+    }
+}
